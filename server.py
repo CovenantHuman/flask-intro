6465600ce2a8ae4441d6a083e8a63ac9d9395a56
Michaela Healton <mshealton@gmail.com>
2022-08-23 12:59:17 -0700

    Up to Further Study

--- server.py
+++ server.py
@@ -18,13 +18,15 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html>
+              <html>Hi! This is the home page.
+              <a href="/hello">Hello</a>
+              </html>"""
 
 
 @app.route('/hello')
 def say_hello():
     """Say hello and prompt for user's name."""
-
     return """
     <!doctype html>
     <html>
@@ -34,7 +36,47 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+          <label for="name">What's your name?</label>
+          <input id="name" type="text" name="person">
+          <label for='compliments'>Choose a compliment:</label>
+          <select id='compliments' name="compliments">
+            <option value='awesome'> Awesome </option>
+            <option value='terrific'> Terrific </option>
+            <option value='fantastic'> Fantastic </option> 
+            <option value='neato'> Neato </option>
+            <option value='fantabulous'> Fantabulous </option>
+            <option value='wowza'> Wowza </option> 
+            <option value='oh-so-not-meh'> Oh-so-not-meh </option> 
+            <option value='brilliant'> Brilliant </option> 
+            <option value='ducky'> Ducky </option> 
+            <option value='coolio'> Coolio </option> 
+            <option value='incredible'> Incredible </option> 
+            <option value='wonderful'> Wonderful </option> 
+            <option value='smashing'> Smashing </option>       
+            <option value='lovely'> Lovely </option>                                          
+          </select>  
+          <input type="submit" value="Submit">
+        </form>
+        <form action="/diss">
+          <label for="name">What's your name?</label>
+          <input id="name" type="text" name="person_too">
+          <label for='disses'>Choose a diss:</label>
+          <select id='disses' name="disses">
+            <option value='not awesome'> Not awesome </option>
+            <option value='not terrific'> not terrific </option>
+            <option value='not fantastic'> not fantastic </option> 
+            <option value='not neato'> not neato </option>
+            <option value='not fantabulous'> not fantabulous </option>
+            <option value='not wowza'> not wowza </option> 
+            <option value='oh-so-meh'> Oh-so-meh </option> 
+            <option value='not brilliant'> not brilliant </option> 
+            <option value='not ducky'> not ducky </option> 
+            <option value='not coolio'> not coolio </option> 
+            <option value='not incredible'> not incredible </option> 
+            <option value='not wonderful'> not wonderful </option> 
+            <option value='not smashing'> not smashing </option>       
+            <option value='not lovely'> not lovely </option>                                                       
+          </select>  
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,7 +90,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliments")
 
     return f"""
     <!doctype html>
@@ -62,6 +104,25 @@ def greet_person():
     </html>
     """
 
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person_too")
+
+    diss = request.args.get("disses")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {diss}!
+      </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
